50938d74e2bf0b033b4abd4f12ddd0862152a9d7
kritgarg <kritg0160@gmail.com>
2025-03-24 10:13:45 +0530

    fixing sklearn library

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import sklearn
 from sklearn.ensemble import RandomForestRegressor
 
 # Load dataset (for similar results section)
